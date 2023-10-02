3bc4247594febf3c0f51fa97ed53bb1b3fe0f8ef
Fabianpolanco <110071437+Fabianpolanco@users.noreply.github.com>
2023-10-02 17:00:13 -0500

    Animacion GSAP aplicada

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,31 +1,49 @@
-// var tl = gsap.timeline({scrollTrigger:{
-//     trigger:"#main",
-//    //  markers:true,
-//     start:"50% 50%",
-//     end:"150% 50%",
-//     scrub:2,
-//     pin:true
-// }});
-// tl
-// .to("#center",{
-//    height: "100vh",
-// },'a')
-// .to("#top",{
-//     top: "-50%",
-//  },'a')
-//  .to("#bottom",{
-//     bottom: "-50%",
-//  },'a')
-// .to("#top-h1",{
-//     top: "60%"
-//  },'a')
-//  .to("#bottom-h1",{
-//     bottom: "-30%"
-//  },'a')
-// .to("#center-h1",{
-//    top: "-30%"
-// },'a')
-// .to(".content",{
-//    delay: -0.2,
-//    marginTop: "0%"
-// })
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#main",
+    //  markers:true,
+    start: "50% 50%",
+    end: "100% 50%",
+    scrub: 2,
+    pin: true,
+  },
+});
+tl.to(
+    "#top",
+    {
+      top: "-50%",
+    },
+    "a"
+  )
+  .to(
+    "#bottom",
+    {
+      bottom: "-50%",
+    },
+    "a"
+  )
+  .to(
+    "#top-h2",
+    {
+      top: "60%",
+    },
+    "a"
+  )
+  .to(
+    "#bottom-h2",
+    {
+      bottom: "-30%",
+    },
+    "a"
+  )
+  .to(
+    "#center-h2",
+    {
+      top: "-30%",
+    },
+    "a"
+  )
+  .to(".content", {
+    delay: -0.2,
+    marginTop: "0%",
+  });
